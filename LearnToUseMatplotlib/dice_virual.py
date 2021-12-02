60661076fc947b3d2ca5b2b7cb3dde1357fafcd6
Will Lee <40391650+MaybeTommorow@users.noreply.github.com>
2021-12-02 10:53:29 +0800

    d6_d10

--- LearnToUseMatplotlib/dice_virual.py
+++ LearnToUseMatplotlib/dice_virual.py
@@ -5,9 +5,9 @@ from dice import Dice
 
 
 dice_1 = Dice()
-dice_2 = Dice()
+dice_2 = Dice(10)
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
 	result = dice_1.roll() + dice_2.roll()
 	results.append(result)
 
@@ -21,6 +21,6 @@ x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'results', 'dtick': 1}
 y_axis_config = {'title': 'frequencies'}
-my_layout = Layout(title="Results of rolling two D6s 1000 times",
+my_layout = Layout(title="Results of rolling one D6 and one D10 50000 times",
 	xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data, 'layout':my_layout}, filename='d6_d6.html')
+offline.plot({'data':data, 'layout':my_layout}, filename='d6_d10.html')
